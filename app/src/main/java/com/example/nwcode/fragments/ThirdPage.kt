1f96a3651d12e3f24fdad77704367cfe9d0b6c14
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-07-22 15:09:05 +0300

    Third page

---
+++ app/src/main/java/com/example/nwcode/fragments/ThirdPage.kt
@@ -0,0 +1,52 @@
+package com.example.nwcode.fragments
+
+import android.app.WallpaperManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.drawToBitmap
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.nwcode.R
+import com.example.nwcode.viewModels.ThirdPageViewModel
+import com.example.nwcode.viewModels.ThirdPageViewModelFactory
+import java.io.IOException
+
+
+class ThirdPage : Fragment(R.layout.fragment_third_page) {
+    private val viewModel : ThirdPageViewModel by activityViewModels { ThirdPageViewModelFactory() }
+    private lateinit var imageView : ImageView
+    private lateinit var button : Button
+    private val arguments : ThirdPageArgs by navArgs()
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.e("Request", arguments.id)
+        viewModel.getPhotoById(arguments.id)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageView = view.findViewById(R.id.thirdPageIV)
+        button = view.findViewById(R.id.thirdPageBtn)
+        viewModel.photoById.observe(viewLifecycleOwner){
+            Glide.with(view).load(it.urls?.full.toString()).into(imageView)
+        }
+        button.setOnClickListener {
+            val bmap : Bitmap = imageView.drawToBitmap()
+            val m = WallpaperManager.getInstance(context)
+            try {
+                m.setBitmap(bmap)
+                Toast.makeText(context, "Wallpaper Set Successfully", Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
