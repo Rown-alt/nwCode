687ea1436b9806da486ca740237e346a2775005d
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2022-07-22 15:09:24 +0300

    First Page

--- app/src/main/java/com/example/nwcode/fragments/FirstPage.kt
+++ app/src/main/java/com/example/nwcode/fragments/FirstPage.kt
@@ -1,60 +1,39 @@
 package com.example.nwcode.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.nwcode.R
+import com.example.nwcode.adapters.CategoryAdapter
+import com.example.nwcode.viewModels.FirstPageViewModel
+import com.example.nwcode.viewModels.FirstPageViewModelFactory
+import com.example.nwcode.viewModels.ThirdPageViewModel
+import com.example.nwcode.viewModels.ThirdPageViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FirstPage : Fragment(R.layout.fragment_first_page) {
+    private val adapter = CategoryAdapter()
+    private lateinit var recyclerCategories : RecyclerView
+    private val viewModel : FirstPageViewModel by activityViewModels { FirstPageViewModelFactory() }
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstPage.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FirstPage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerCategories = view.findViewById(R.id.categories_RV)
+        viewModel.collections.observe(viewLifecycleOwner){
+            adapter.setCategories(it)
         }
+        recyclerCategories.adapter = adapter
+        recyclerCategories.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_page, container, false)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        viewModel.getCollections()
+        Log.e("Request", "Sending ")
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstPage.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FirstPage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
